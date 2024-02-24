428b7f171b742802ad33f30543985ad99a08ff5f
rimuyamaura <148062753+rimuyamaura@users.noreply.github.com>
2024-02-24 18:00:18 +1300

    Deployed site, updated contacts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,12 +7,19 @@ const Contact = () => {
         <h1 className='text-5xl xl:text-9xl pb-[20%]'>CONTACT</h1>
       </div>
       <div>
-        <p className='text-xs xl:text-base py-[5%]'>
-          <a href='mailto:rimuyamaura@gmail.com'>rimuyamaura@gmail.com</a>
+        <p className='text-xs xl:text-base py-[5%] '>
+          <a href='mailto:rimuyamaura@gmail.com' className='hover:text-warning'>
+            rimuyamaura@gmail.com
+          </a>
         </p>
         <p className='text-xs xl:text-base py-[5%]'>(+86) 022 516 7123</p>
         <p className='text-xs xl:text-base py-[5%]'>
-          <a href={pdf} target='_blank' rel='noreferrer'>
+          <a
+            href={pdf}
+            className='hover:text-warning'
+            target='_blank'
+            rel='noreferrer'
+          >
             View my resume HERE
           </a>
         </p>
