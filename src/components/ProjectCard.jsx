302409d2116b541a5952742b6d4c088c29b13add
rimuyamaura <148062753+rimuyamaura@users.noreply.github.com>
2024-04-30 19:52:03 +1200

    update projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,36 +3,40 @@ import { FaGithubSquare } from 'react-icons/fa';
 
 import { useState } from 'react';
 
-const ProjectCard = ({ url, img, github, title, text }) => {
+const ProjectCard = ({ url, img, imgFocus, github, title, text }) => {
   const [imgPopup, setImgPopup] = useState(null);
 
   return (
     <>
-      <div className='flex flex-col items-center sm:flex-row sm:h-[162px] border hover:bg-base-300 hover:bg-opacity-80'>
+      <div className='flex flex-col items-center sm:flex-row sm:h-[150px] border hover:bg-base-300 hover:bg-opacity-80'>
         <img
-          className='object-cover object-left w-full h-auto sm:h-40 sm:w-24 hover:scale-110 duration-500 cursor-pointer'
+          className={`object-cover w-full h-auto sm:h-[148px] sm:w-24 hover:scale-110 duration-500 cursor-pointer ${
+            imgFocus === null ? 'object-left' : ''
+          }`}
           src={img}
           alt={title}
           onClick={() => setImgPopup(img)}
         />
-        <div className='flex flex-col justify-between p-4 w-full h-auto'>
+        <div className='flex flex-col p-4 w-full h-auto'>
           <h1 className='mb-2 text-2xl xl:text-3xl uppercase'>{title}</h1>
-          <p className='mb-3 text-xs text-balance'>{text}</p>
-          <div className='flex justify-end  gap-x-4'>
-            {github !== '' ? (
-              <a href={github} target='_blank' rel='noreferrer'>
-                <FaGithubSquare className='h-5 w-5 text-slate-500 hover:text-warning duration-300' />
-              </a>
-            ) : (
-              ''
-            )}
-            {url !== '' ? (
-              <a href={url} target='_blank' rel='noreferrer'>
-                <FiExternalLink className='h-5 w-5 text-slate-500 hover:text-warning duration-300' />
-              </a>
-            ) : (
-              ''
-            )}
+          <div className='grid sm:grid-cols-5 gap-1'>
+            <p className='col-span-4 mb-2 text-xs'>{text}</p>
+            <div className='col-span-1 flex justify-end items-end gap-x-2'>
+              {github !== '' ? (
+                <a href={github} target='_blank' rel='noreferrer'>
+                  <FaGithubSquare className='h-5 w-5 text-slate-500 hover:text-warning duration-300' />
+                </a>
+              ) : (
+                ''
+              )}
+              {url !== '' ? (
+                <a href={url} target='_blank' rel='noreferrer'>
+                  <FiExternalLink className='h-5 w-5 text-slate-500 hover:text-warning duration-300' />
+                </a>
+              ) : (
+                ''
+              )}
+            </div>
           </div>
         </div>
       </div>
--- src/data.jsx
+++ src/data.jsx
@@ -3,19 +3,31 @@ import terramagotchiImg from './assets/terramagotchi.png';
 import portfolioImg from './assets/portfolio.png';
 import comfyImg from './assets/comfy.png';
 import musicImg from './assets/music.png';
+import socialImg from './assets/social.png';
 
 export const projects = [
   {
     id: nanoid(),
     img: terramagotchiImg,
+    imgFocus: null,
     url: 'https://terramagotchi-trofik-edition.web.app',
     github: '',
     title: 'Terramagotchi trofik edition',
-    text: 'Interactive simulation where you can observe and engage with the process of plant growth in soil. Created using Javascript and Google Firebase backend.',
+    text: 'Interactive JavaScript simulation where you can engage with the process of plant growth in soil. Using Google Firebase backend.',
+  },
+  {
+    id: nanoid(),
+    img: socialImg,
+    imgFocus: 'center',
+    url: 'https://social-app-b8kg.onrender.com',
+    github: 'https://github.com/rimuyamaura/social-app',
+    title: 'Social-App',
+    text: 'React Social media application with realtime chat function. Backend built using Express.js and MongoDB',
   },
   {
     id: nanoid(),
     img: comfyImg,
+    imgFocus: 'center',
     url: 'https://ry-comfy-store.netlify.app',
     github: 'https://github.com/rimuyamaura/comfy-store',
     title: 'Comfy Store',
@@ -24,6 +36,7 @@ export const projects = [
   {
     id: nanoid(),
     img: portfolioImg,
+    imgFocus: null,
     url: 'https://rimuyamaura.netlify.app',
     github: 'https://github.com/rimuyamaura/rimuyamaura-portfolio',
     title: 'Portfolio Website',
@@ -32,6 +45,7 @@ export const projects = [
   {
     id: nanoid(),
     img: musicImg,
+    imgFocus: null,
     url: '',
     github: 'https://github.com/rimuyamaura/music-library',
     title: 'Music Library',
